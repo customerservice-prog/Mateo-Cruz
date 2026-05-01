d4c0f57ff62a0b6c8beaea03d8156d57e3272a22
customerservice-prog <customerservice@friendlypartyrental.com>
2026-05-01 06:14:14 -0400

    Create next.config.js

---
+++ apps/web/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.r2.cloudflarestorage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.r2.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: 'replicate.delivery',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+};
+
+module.exports = nextConfig;
